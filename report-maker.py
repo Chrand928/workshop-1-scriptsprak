a91f9aa8572a1b5e9c29136fe6037386d5de8497
Chrand928 <andreasson.chr@gmail.com>
2025-10-01 14:22:37 +0200

    Testing loops for offline/warning devices

--- report-maker.py
+++ report-maker.py
@@ -7,20 +7,29 @@ data = json.load(open("network-devices.json","r",encoding = "utf-8"))
 # Create a variable that holds our whole text report
 report = ""
 report += "===============\n" + "Nätverksrapport" + "\n===============\n"
+report += "\n" + "ENHETER MED PROBLEM" + "\n---------------\n"
 
-# loop through the "locations" list
-for location in data["locations"]:
-    # print the "site/name" of the location
-    
-    report += "\n" + "ENHETER MED PROBLEM" + "\n---------------\n"
-    # print a list of the host names of the devices on the location
-    
+# Loop to collect all "offline" devices in a list
+report += "\nStatus: OFFLINE\n"
+for location in data["locations"]:   
     for device in location["devices"]:
         if device["status"] == "offline":
-            report += device["hostname"] + " " 
-            report += device["ip_address"] + " "
-            report += device["type"] + " "
-            report += device["status"] + "\n"
+            report += device["hostname"] + "   " 
+            report += device["ip_address"] + "   "
+            report += device["type"] + "   "
+            report += location["site"] + "\n"
+
+# Loop to collect all "warning" devices in a list
+report += "\nStatus: WARNING\n"
+for location in data["locations"]:
+    for device in location["devices"]:
+        if device["status"] == "warning":
+            report += device["hostname"] + "   " 
+            report += device["ip_address"] + "   "
+            report += device["type"] + "   "
+            report += location["site"] + "   "
+            report += "Uptime days: " + str(device["uptime_days"])
+            report += "\n"
 
 # Write the report to textfile
 with open ("report.txt", "w", encoding="utf-8") as f:
